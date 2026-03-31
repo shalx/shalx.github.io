534081bf31d4059fa82b983edfa1b2dbd0d7e218
shalx <shalva@inbox.com>
2026-03-31 08:12:56 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE = "todo-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
