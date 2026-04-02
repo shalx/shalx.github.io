fb97e7b5eaea5c5ea89eb7708bbc5722da9a1f9a
shalx <shalva@inbox.com>
2026-04-02 07:01:12 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,13 @@
-const CACHE_NAME = "todo-pwa-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE="todo-v2";
 
-// install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(["./","./index.html"]))
   );
 });
 
-// activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-});
-
-// fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
   );
 });
