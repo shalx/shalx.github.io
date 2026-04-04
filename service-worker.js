08704f17fc05f29dc8fed612aa2abce95701450a
shalx <shalva@inbox.com>
2026-04-04 08:18:14 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "savefoun-pwa-v1";
+const CACHE_NAME = "savefound-pwa-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -12,19 +12,17 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
 // activate
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
     )
   );
+  self.clients.claim();
 });
 
 // fetch
