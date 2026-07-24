e19ed823a4b8893b29e472ce79ff117a28029dd0
shalx <shalva@inbox.com>
2026-07-24 06:39:05 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,79 @@
-const CACHE_NAME="fix pin-cache-v1";
-const urlsToCache=["/","index.html","manifest.json","https://unpkg.com/leaflet@1.9.4/dist/leaflet.js","https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"];
+"use strict";
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+const CACHE_NAME = "fix-pin-cache-v2";
+
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+];
+
+
+// INSTALL
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
   );
+
   self.skipWaiting();
+
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(response=>response||fetch(e.request))
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+
+        return Promise.all(
+          cacheNames.map(cacheName => {
+
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+
+          })
+        );
+
+      })
   );
+
+  self.clients.claim();
+
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+
+      })
+  );
+
 });
